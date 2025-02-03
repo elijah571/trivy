cbc30c12ff063fc7415cf1e5e7b292e4b603e3cf
elijah571 <elijahfx43@gmail.com>
2025-02-03 06:10:12 -0800

    base url

--- Frontend/src/redux/constant.js
+++ Frontend/src/redux/constant.js
@@ -1,5 +1,5 @@
 // Define the base URL for the backend server
-export const BASE_URL = "";  
+export const BASE_URL = "https://trivy-nr2vb74sj-elijahs-projects-e0099976.vercel.app/";  
 // Define the API endpoint URLs
 export const TEACHERS_URL = "/api/teachers";
 export const STUDENTS_URL = "/api/students";
